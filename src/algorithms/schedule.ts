b056f3b13056860ef7251ddc640dfccb94d46d9b
Sam Vervaeck <samvv@pm.me>
2021-02-22 13:41:27 +0100

    Add an async version of schedule()

--- src/algorithms/schedule.ts
+++ src/algorithms/schedule.ts
@@ -1,8 +1,12 @@
 
-import { Graph } from "../graph";
+import { Graph, AsyncGraph } from "../graph";
 
 function isEmpty(iterable: Iterable<any>): boolean {
   return iterable[Symbol.iterator]().next().done!;
+}
+
+async function isEmptyAsync(iterable: AsyncIterable<any>): Promise<boolean> {
+  return (await iterable[Symbol.asyncIterator]().next()).done!;
 }
 
 export function* schedule<V>(graph: Graph<V>): Generator<V[]> {
@@ -21,6 +25,31 @@ export function* schedule<V>(graph: Graph<V>): Generator<V[]> {
     yield group;
     for (const v of group) {
       for (const w of graph.getIncoming(v)) {
+        newGroup.push(w);
+      }
+    }
+    group = newGroup;
+    newGroup = [];
+  }
+
+}
+
+export async function* scheduleAsync<V>(graph: AsyncGraph<V>): AsyncGenerator<V[]> {
+
+  let group = [];
+
+  for await (const v of graph.getVertices()) {
+    if (await isEmptyAsync(graph.getOutgoing(v))) {
+      group.push(v);
+    }
+  }
+
+  let newGroup: V[] = [];
+
+  while (group.length > 0) {
+    yield group;
+    for (const v of group) {
+      for await (const w of graph.getIncoming(v)) {
         newGroup.push(w);
       }
     }
